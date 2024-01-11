103f8988c9bcc729e662dab7b13e803ef3cf290c
Sankarrrrr <119133620+Sankarrrrr@users.noreply.github.com>
2024-01-11 06:41:57 +0300

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/sankar/mediaplayerapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/sankar/mediaplayerapp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("uz.orifjon.mediaplayerapp", appContext.packageName)
+        assertEquals("com.sankar.mediaplayerapp", appContext.packageName)
     }
 }
